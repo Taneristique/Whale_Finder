a4a6d5b97af49d2feabe3c524ff123000dc6fc98
Taneristique <grkmtnrackz@outlook.fr>
2022-08-14 21:31:56 +0300

    v1.0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import WhaleFinder from './whalefinder';
 import React, { Component } from 'react';
+import { ethers } from 'ethers';
 import ErrorPage from './component/ErrorPage';
 
 export default class App extends Component{
@@ -11,31 +12,51 @@ export default class App extends Component{
       formSending:false,
       validation:false,
       hasError:false,
-      isChecked:false
+      USDT:{
+        address:'0xdAC17F958D2ee523a2206206994597C13D831ec7',
+        abi:[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"upgradedAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"maximumFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_maker","type":"address"}],"name":"getBlackListStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newBasisPoints","type":"uint256"},{"name":"newMaxFee","type":"uint256"}],"name":"setParams","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"issue","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"redeem","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"basisPointsRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}],
+        default_amount:100000*10**6
+      },
+      BNB:{
+        address:'0xB8c77482e45F1F44dE1745F52C74426C631bDD52',
+        abi:[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdrawEther","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"unfreeze","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"freezeOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"freeze","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Freeze","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Unfreeze","type":"event"}],
+        default_amount:ethers.utils.parseEther("50.0")
+      }
       }
   }  
-  handleOnChange = () => {
-    this.state.isChecked===false? this.setState({isChecked:true}):this.setState({isChecked:false})
-  };
-   inputChange=(e)=>{
+ 
+  inputChange=(e)=>{
       this.setState({[e.target.name]:e.target.value})
     }
-     formSubmit=()=>{
+  formSubmit=()=>{
       this.setState({formSending : true})
       setTimeout(()=>{
         this.setState({formSending:false,validation:true})}
         ,1000)
       
     }
+  loadInformation=(e)=>{
+    if(e.target.value==="usdt"){
+      this.setState({CONTRACT_ADDRESS:this.state.USDT.address,ABI:this.state.USDT.abi,TRESHOLD:this.state.USDT.default_amount})
+    }
+    else if(e.target.value==="bnb"){
+      this.setState({CONTRACT_ADDRESS:this.state.BNB.address,ABI:this.state.BNB.abi,TRESHOLD:this.state.BNB.default_amount})
+    }
+    else{
+      this.setState({ CONTRACT_ADDRESS:'',ABI:'',TRESHOLD:''})
+    }
+  }
+
     componentDidCatch(error, info) {
       // Display fallback UI
       this.setState({ hasError: true });
       // You can also log the error to an error reporting service
       console.log(error, info);
     }
+
    
   render(){
-    let {CONTRACT_ADDRESS,ABI,TRESHOLD,formSending,validation}=this.state
+    let {CONTRACT_ADDRESS,ABI,TRESHOLD,formSending,validation}=this.state;
     return (
          formSending?
          <div className='w-screen h-screen bg-black grid place-content-center ...'>
@@ -60,29 +81,28 @@ export default class App extends Component{
       </svg>
          </div>
         :this.state.hasError?
-          <ErrorPage/>
+            <ErrorPage/>
         :validation?
-          this.state.isChecked?
             <div>
-                    <WhaleFinder address={CONTRACT_ADDRESS} abi={ABI} transferTreshold={TRESHOLD} check={1}/>
+                    <WhaleFinder address={CONTRACT_ADDRESS} abi={ABI} transferTreshold={TRESHOLD}/>
             </div>
-          :
-          <div className='bg-black w-screen h-screen text-center  text-red-50  grid place-content-center ...'>
-            <WhaleFinder address={CONTRACT_ADDRESS} abi={ABI} transferTreshold={TRESHOLD}/>
-          </div>
           :
           <div className='w-screen h-screen bg-black  grid place-content-center ...'> 
           <h1 className='text-red-50 mb-5 text-2xl'>Welcome To Whale Finder</h1>
           <form  className="flex flex-col items-center rounded-sm"   onSubmit={this.formSubmit}>
-          <label className=" text-center text-red-50" htmlFor='tokenContract'>Token Contract Address</label>
-          <input  className="bg-slate-400 rounded-sm" name='CONTRACT_ADDRESS' id="tokenContract" value={CONTRACT_ADDRESS} onChange={this.inputChange}/><br/>
-          <label className="text-center text-red-50" htmlFor='tokenAbi'>Token ABI</label>
-          <input className="bg-slate-400  rounded-sm" name='ABI' id='tokenAbi' value={ABI} onChange={this.inputChange}/><br/>
-          <label  className="text-center text-red-50" htmlFor='txTreshold'>Transfer Treshold</label>
-          <input className="bg-slate-400 rounded-sm" name='TRESHOLD' id="txTreshold"value={TRESHOLD} onChange={this.inputChange}/><br/>
-          <label className=" text-center mt-5 text-red-50" htmlFor='MobileDeviceVersion'>Use Mobile Version</label>
-          <input type="checkbox"  name="mobileVersion_button" value="mobile" id='MobileDeviceVersion' checked={this.state.isChecked} onChange={this.handleOnChange}/>
-          <input className='text-black bg-white hover:bg-green-200 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:ring-gray-500 dark:hover:bg-green-300 dark:focus:ring-green-400 mt-3' type='submit' value="submit"/>
+            <label className=" text-center text-red-50" htmlFor='tokenContract'>Default Tokens</label>
+            <select defaultValue={"default"} className="text-center bg-slate-400 rounded-sm" onChange={this.loadInformation}>
+              <option value="usdt">USDT</option>
+              <option value="bnb">BNB</option>
+              <option value="default">DEFAULT</option>
+            </select>
+            <label className=" text-center text-red-50" htmlFor='tokenContract'>Token Contract Address</label>
+            <input  className="bg-slate-400 rounded-sm" name='CONTRACT_ADDRESS' id="tokenContract"  value={CONTRACT_ADDRESS} onChange={this.inputChange}/><br/>
+            <label className="text-center text-red-50" htmlFor='tokenAbi'>Token ABI</label>
+            <input className="bg-slate-400  rounded-sm" name='ABI' id='tokenAbi' value={ABI} onChange={this.inputChange}/><br/>
+            <label  className="text-center text-red-50" htmlFor='txTreshold'>Transfer Treshold</label>
+            <input className="bg-slate-400 rounded-sm" name='TRESHOLD' id="txTreshold"value={TRESHOLD} onChange={this.inputChange}/><br/>
+            <input className='text-black bg-white hover:bg-green-200 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:ring-gray-500 dark:hover:bg-green-300 dark:focus:ring-green-400 mt-3' type='submit' value="submit"/>
           </form> 
           </div>
   );
--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -4,8 +4,8 @@ export default class ErrorPage extends React.Component{
         const url='https://github.com/Taneristique/Whale_Finder/issues'
         const githubPage=<a href={url} className='text-teal-300'>https://github.com/Taneristique/Whale_Finder</a>
         return(
-            <div className='bg-black w-screen h-screen text-center  text-red-50  grid place-content-center ...'>
-            <p className='whitespace-pre-line truncate	'>Operation could not realize,please check your console for more details.<br/>
+            <div className='w-screen h-screen text-center  text-red-50  grid place-content-center ...'>
+            <p className='whitespace-pre-line truncate	 text-red-50'>Operation could not realize,please check your console for more details.<br/>
             In case you do not have access to the console <br/>
              please check your network connection and login parameters after refreshing the page and retry to login.<br/>Else try fixing the error using  error information 
              than retry to login.<br/>
